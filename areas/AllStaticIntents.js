d87b2db6632ea76893c46bcbe794b1565858edf2
sumit rudra <sumitrudra@gmail.com>
2019-11-06 14:24:32 +0530

    modified by sumit on 06112019

--- areas/AllStaticIntents.js
+++ areas/AllStaticIntents.js
@@ -11,6 +11,8 @@ const AllStaticIntents = {
             || handlerInput.requestEnvelope.request.intent.name === 'GVSUEmergencyAbroad'
             || handlerInput.requestEnvelope.request.intent.name === 'GVSUParking'
             || handlerInput.requestEnvelope.request.intent.name === 'ComputerIssues'
+            || handlerInput.requestEnvelope.request.intent.name === 'FinncialAidApplicationDeadline'
+            || handlerInput.requestEnvelope.request.intent.name === 'FinncialAidApplicationTime'
         );
     },
     handle(handlerInput) {
